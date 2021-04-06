b7aa1518ab644ec609c0ca2f316f4ebc3b8e0d60
Carlos Lopez A <celpzar@gmail.com>
2021-04-05 21:30:16 -0400

    Adding counting code for string inputs

--- Python-Basics/dictionary.py
+++ Python-Basics/dictionary.py
@@ -7,3 +7,21 @@ for ext, amount in file_counts.items():
 
 print(file_counts.keys())
 print(file_counts.values())
+
+cool_beasts = {"octopuses":"tentacles", "dolphins":"fins", "rhinos":"horns"}
+for animal, arms in cool_beasts.items():
+    print("{} have {}".format(animal, arms))
+
+# Counting Occurences of letters in text
+def count_letters(text):
+    result = {}
+    for letter in text:
+        if letter not in result:
+            result[letter] = 0
+        result[letter] += 1
+    return print(result)
+
+count_letters("AAAAAAAAAAAAAAAA")
+count_letters("TESTIMONYAGAOINTTTT")
+
+#dictionary vs list
