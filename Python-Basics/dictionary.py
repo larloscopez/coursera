2f94ac1069247e0933726a3cf0547fca1e355a99
Carlos Lopez A <celpzar@gmail.com>
2021-04-05 21:09:21 -0400

    commit

--- Python-Basics/dictionary.py
+++ Python-Basics/dictionary.py
@@ -1,3 +1,9 @@
 file_counts = {"jpg":10, "txt":14, "csv":2, "py":23}
 for extension in file_counts:
     print(extension)
+
+for ext, amount in file_counts.items():
+    print("There are {} of {} files".format(amount, ext))
+
+print(file_counts.keys())
+print(file_counts.values())
